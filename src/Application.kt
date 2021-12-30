5e65d9c886e79619a623e6a74339f6199d92ef02
arcelorapp@123 <arcelorapp@gmail.com>
2021-12-30 10:28:03 +0530

    host text crud data

--- src/Application.kt
+++ src/Application.kt
@@ -1,6 +1,5 @@
 package com.codexdroid
 
-import com.google.gson.Gson
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.request.*
@@ -10,10 +9,9 @@ import io.ktor.gson.*
 import io.ktor.features.*
 import kotlinx.serialization.Serializable
 import org.ktorm.database.Database
-import org.ktorm.dsl.insert
+import org.ktorm.dsl.*
 import org.ktorm.schema.Table
 import org.ktorm.schema.int
-import org.ktorm.schema.long
 import org.ktorm.schema.varchar
 import kotlin.random.Random
 
@@ -33,7 +31,7 @@ object PersonTable : Table<Nothing>(tableName = "person"){          // This clas
 fun getDatabase() : Database? {
     database = if(database == null){
         Database.connect(
-            url = "jdbc:mysql://test-db-do-user-10480409-0.b.db.ondigitalocean.com",
+            url = "jdbc:mysql://test-db-do-user-10480409-0.b.db.ondigitalocean.com:25060/defaultdb",  //jdbc:hostname.com:port/dabasename
             driver = "com.mysql.cj.jdbc.Driver",
             user = "doadmin",
             password = "9AvhLIGrecrpdHfl")
@@ -45,33 +43,48 @@ fun getDatabase() : Database? {
 @Serializable
 data class Person(var id:Int,var name: String,var email:String,var mobile:String)
 
-
-
 @Serializable
-data class UserData(var status: Int, var message: String, var person : Person)
+data class UserData(var status: Int, var message: String, var person : MutableList<Person>)
+
+val personList = mutableListOf<Person>()
 
 @Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
+fun Application.module() {
     install(ContentNegotiation) {
         gson {}
     }
 
     routing {
         get("/username/{name}"){
-            val name = call.parameters["name"] ?: call.respond(HttpStatusCode.BadRequest,"Enter String only");
+            val name = call.parameters["name"] ?: call.respond(HttpStatusCode.BadRequest,"Enter String only")
             call.respond(HttpStatusCode.OK,"Hello $name, How are you?")
         }
 
         get("/email/{email}"){
-            val email = call.parameters["email"] ?: call.respond(HttpStatusCode.BadRequest,"Enter String only");
+            val email = call.parameters["email"] ?: call.respond(HttpStatusCode.BadRequest,"Enter String only")
             call.respond(HttpStatusCode.OK,"Hello $email, We mail you Secret Key, Please Delete It with in 1 Second ")
+        }
+
+        get("/info"){
+            call.respond("No Information available for now, pls keep visiting")
+        }
+
+        get("/"){
+            Random.nextInt()
+            var fname = ""
+            var lname = ""
+            repeat(6){
+                fname += Random.nextInt(65,65+26).toChar().toString()
+                lname += Random.nextInt(97,97+26).toChar().toString()
+            }
+            personList.add(Person(Random.nextInt(100,999),"$fname $lname","${fname}@codexdroid.com",Random.nextLong(1111111111,9999999999).toString()))
+            call.respond(UserData(HttpStatusCode.OK.value,"Here's Your Data", personList))
         }
 
         post("/data"){
             val person = call.receive<Person>()
 
-            println("====> ${call.request.authorization()}")
+            println("====> ${call.request.authorization()}")  //Bearer Token
 
             val affectedRows = getDatabase()?.insert(PersonTable){
                 set(PersonTable.id,person.id)
@@ -85,26 +98,74 @@ fun Application.module(testing: Boolean = false) {
             }else{
                 call.respond("Data Could not insert")
             }
-
-
-            call.respond(HttpStatusCode.OK,person)
         }
 
-        get("/"){
-            Random.nextInt()
-            var fname = ""
-            var lname = ""
-            repeat(6){
-                fname += Random.nextInt(65,65+26).toChar().toString()
-                lname += Random.nextInt(97,97+26).toChar().toString()
+        get("/view"){
+            personList.clear()
+            val query = getDatabase()?.from(PersonTable)?.select()
+            for (row in query!!){
+                personList.add(Person(row[PersonTable.id]!!,row[PersonTable.name]!!, row[PersonTable.email]!!, row[PersonTable.mobile]!!))
+            }
+            call.respond(UserData(HttpStatusCode.OK.value,"Data Retrieved Successfully",personList))
+        }
+
+        put("/update_object"){
+            val person = call.receive<Person>()
+            val affectedRows = getDatabase()?.update(PersonTable){
+                set(PersonTable.name,person.name)
+                set(PersonTable.email,person.email)
+                set(PersonTable.mobile,person.mobile)
+                where { PersonTable.id eq person.id }
             }
 
-            val person = Person(Random.nextInt(100,999),"$fname $lname","${fname}@codexdroid.com",Random.nextLong(1111111111,9999999999).toString())
-            call.respond(UserData(HttpStatusCode.OK.value,"Here's Your Data",person))
+            if(affectedRows!! > 0){
+                call.respond("Data Successfully Updated")
+            }else{
+                call.respond("Error to Updated Data")
+            }
         }
 
-        get("/info"){
-            call.respond("No Information available for now, pls keep visiting")
+        put("/update_id/{id}"){
+            val id = call.parameters["id"].toString().toInt();
+            val person = call.receive<Person>()
+
+            val affectedRows = getDatabase()?.update(PersonTable){
+                set(PersonTable.name,person.name)
+                set(PersonTable.email,person.email)
+                set(PersonTable.mobile,person.mobile)
+                where { PersonTable.id eq id }
+            }
+
+            if(affectedRows!! > 0){
+                call.respond("Data Successfully Updated of id = $id")
+            }else{
+                call.respond("Error to Updated Data")
+            }
+        }
+
+        delete("/delete_object"){
+            val person = call.receive<Person>()
+            val affectedRows = getDatabase()?.delete(PersonTable){
+                PersonTable.id eq person.id
+            }
+            if(affectedRows!! > 0){
+                call.respond("Data Successfully Deleted")
+            }else{
+                call.respond("Error to Delete Data")
+            }
+        }
+
+        delete("/delete_id/{id}"){
+            val id = call.parameters["id"].toString().toInt();
+            val affectedRows = getDatabase()?.delete(PersonTable){
+                PersonTable.id eq id
+            }
+
+            if(affectedRows!! > 0){
+                call.respond("Data Successfully Deleted")
+            }else{
+                call.respond("Error to Delete Data")
+            }
         }
     }
 }
--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -1,16 +1,6 @@
 package com.codexdroid
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
 import io.ktor.http.*
-import io.ktor.gson.*
-import io.ktor.features.*
-import io.ktor.client.*
-import io.ktor.client.features.json.*
-import io.ktor.client.request.*
-import kotlinx.coroutines.*
 //import kotlin.test.*
 import io.ktor.server.testing.*
 import junit.framework.Assert.assertEquals
@@ -19,7 +9,7 @@ import org.junit.Test
 class ApplicationTest {
     @Test
     fun testRoot() {
-        withTestApplication({ module(testing = true) }) {
+        withTestApplication({ module() }) {
             handleRequest(HttpMethod.Get, "/").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
                 assertEquals("HELLO WORLD!", response.content)
